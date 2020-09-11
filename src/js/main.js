30e48c2caf5cce7076439a3d4f56cca87337756b
starseat <starseat3@gmail.com>
2020-09-11 18:37:14 +0900

    메인 화면 작업중 (틀 완성)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 function goGroupItem(item) {
     alert('goGroupItem - ' + item);
 }
+
+$(document).ready(function() {
+    $(window).scroll(function () {
+        var height = $(document).scrollTop();
+        if(height > 10) {            
+            $('#hjfr-navbar-section').css({ backgroundColor: 'rgba(255, 255, 255, 1)' });
+        }
+        else {
+            $('#hjfr-navbar-section').css({ backgroundColor: 'rgba(255, 255, 255, 0)' });
+        }
+    });
+});
+
