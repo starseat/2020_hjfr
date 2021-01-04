cc53138204fbe791a4cc8f647f018d069d935e30
jw.lee <starseat3@gmail.com>
2021-01-04 22:42:29 +0900

    index 페이지의 첫번째 화면 꽉차게 수정

--- src/js/main.js
+++ src/js/main.js
@@ -12,5 +12,16 @@ $(document).ready(function() {
             $('#hjfr-navbar-section').css({ backgroundColor: 'rgba(255, 255, 255, 0)' });
         }
     });
+
+    fullMain();
+    $(window).resize(function() { fullMain(); });
 });
+
+function fullMain() {
+    var minusHeight = $('#hjfr-navbar-section').outerHeight();
+    if(window.innerWidth < 990) {
+        minusHeight = 82;
+    }
+    $('#hjfr-main').css({height: window.innerHeight - minusHeight });
+}
 
