58b8608660ef16029b0bf052e867962f46d79e5a
jw.lee <starseat3@gmail.com>
2021-01-05 01:24:20 +0900

    이미지 슬라이드 처리 수정

--- src/js/group.js
+++ src/js/group.js
@@ -14,6 +14,7 @@ var swiper_option = {
     centeredSlides: true,
     lazy: true,
     loop: true,
+    //roundLengths: true, //일반 해상도 화면에서 텍스트가 흐려지는 것을 방지하기 위해 슬라이드 너비와 높이 값을 반올림
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
